12a5ee2106ba79f711ed02ddfdeffd23070255f1
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2016-05-04 08:25:14 +0300

    Encode and send json messages

--- src/SpaceBlitz/Server.purs
+++ src/SpaceBlitz/Server.purs
@@ -3,14 +3,16 @@ module SpaceBlitz.Server where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE(), log)
-import Data.Argonaut.Core (toObject, toString)
-import Data.Argonaut.Decode (DecodeJson, decodeJson, gDecodeJson)
-import Data.Argonaut.Encode (EncodeJson, encodeJson, gEncodeJson)
+import Data.Argonaut.Core (fromObject, fromString, toObject, toString)
+import Data.Argonaut.Decode (class DecodeJson, decodeJson)
+import Data.Argonaut.Encode (class EncodeJson, encodeJson)
 import Data.Argonaut.Parser (jsonParser)
+import Data.Argonaut.Printer (printJson)
 import Data.Either (Either(Left, Right))
 import Data.Generic (class Generic, gShow)
 import Data.Maybe (Maybe(Just, Nothing), maybe)
 import Data.StrMap as M
+import Data.Tuple (Tuple(Tuple))
 
 import SpaceBlitz.Dgram (UdpEvent(MessageEvent), UDP, createSocket, send)
 
@@ -31,6 +33,7 @@ data Message = FooMessage | BarMessage
 derive instance genericMessage :: Generic Message
 instance showMessage :: Show Message where
   show = gShow
+  
 instance decodeJsonMessage :: DecodeJson Message where
   decodeJson json = maybe (Left $ "Failed to parse message: " ++ show json) Right $ do
     obj <- toObject json
@@ -39,6 +42,10 @@ instance decodeJsonMessage :: DecodeJson Message where
       "foo" -> Just FooMessage
       "bar" -> Just BarMessage
       _ -> Nothing
+      
+instance encodeJsonMessage :: EncodeJson Message where
+  encodeJson FooMessage = fromObject $ M.fromFoldable [Tuple "msgType" $ fromString "foo"]
+  encodeJson BarMessage = fromObject $ M.fromFoldable [Tuple "msgType" $ fromString "bar"]
 
 logMessage :: forall e. Message -> Eff (console :: CONSOLE | e) Unit
 logMessage msg = log $ show msg
@@ -53,5 +60,5 @@ parseMessage raw = jsonParser raw >>= decodeJson
 main :: forall e. Eff (console :: CONSOLE, udp :: UDP| e) Unit
 main = do 
   socket <- createSocket config.port (parseEvent >>> show >>> log)
-  send socket config.host config.hostPort "Hi there!"
+  send socket config.host config.hostPort (printJson $ encodeJson FooMessage)
   return unit
