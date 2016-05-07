46e8a6b25f4417dea47bb0c034cb19dd374686d3
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2016-05-07 22:12:14 +0300

    More involved manual parsing example

--- src/SpaceBlitz/Message.purs
+++ src/SpaceBlitz/Message.purs
@@ -1,5 +1,10 @@
 module SpaceBlitz.Message 
-  ( Message(FooMessage, BarMessage)
+  ( ClientMessage(..)
+  , ServerMessage(..)
+  , GameInitData
+  , MissileData
+  , FleetDirections
+  , FleetStatus
   , parseEvent
   , parseMessage
   ) where
@@ -7,7 +12,7 @@ module SpaceBlitz.Message
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE(), log)
-import Data.Argonaut.Core (fromObject, fromString, toObject, toString)
+import Data.Argonaut.Core (Json, fromObject, fromString, toArray, toObject, toString)
 import Data.Argonaut.Decode (class DecodeJson, decodeJson)
 import Data.Argonaut.Encode (class EncodeJson)
 import Data.Argonaut.Parser (jsonParser)
@@ -16,35 +21,81 @@ import Data.Either (Either(Left, Right))
 import Data.Generic (class Generic, gShow)
 import Data.Maybe (Maybe(Just, Nothing), maybe)
 import Data.StrMap as M
+import Data.Traversable (sequence)
 import Data.Tuple (Tuple(Tuple))
 
 import SpaceBlitz.Dgram (UdpEvent(MessageEvent))
+
+data ServerMessage = 
+     GameInit GameInitData
+   | FleetStatuses (Array FleetStatus)
+   | Pong
+   
+type GameInitData = String
+newtype FleetStatus = FleetStatus
+  { name :: String
+  , status :: Status }
   
-data Message = FooMessage | BarMessage
-derive instance genericMessage :: Generic Message
-instance showMessage :: Show Message where
+data Status = Alive | Destroyed
+   
+data ClientMessage = 
+     Join
+   | Ping
+   | LaunchMissiles MissileData
+   | MoveFleet FleetDirections
+   | SelfDestruct
+   
+type MissileData = String
+type FleetDirections = String
+
+derive instance genericStatus :: Generic Status
+derive instance genericFleetStatus :: Generic FleetStatus
+derive instance genericServerMessage :: Generic ServerMessage
+instance showServerMessage :: Show ServerMessage where
   show = gShow
   
-instance decodeJsonMessage :: DecodeJson Message where
+instance decodeJsonServerMessage :: DecodeJson ServerMessage where
   decodeJson json = maybe (Left $ "Failed to parse message: " ++ show json) Right $ do
     obj <- toObject json
-    msgType <- M.lookup "msgType" obj >>= toString
+    msgType <- M.lookup "type" obj >>= toString
     case msgType of
-      "foo" -> Just FooMessage
-      "bar" -> Just BarMessage
+      "gameInit" -> Just $ GameInit "nothing"
+      "fleetStatuses" -> do
+        statusesArr <- (M.lookup "statuses" obj) >>= toArray
+        statuses <- sequence $ map parseFleetStatus statusesArr
+        Just $ FleetStatuses statuses
+      "pong" -> Just Pong
       _ -> Nothing
       
-instance encodeJsonMessage :: EncodeJson Message where
-  encodeJson FooMessage = fromObject $ M.fromFoldable [Tuple "msgType" $ fromString "foo"]
-  encodeJson BarMessage = fromObject $ M.fromFoldable [Tuple "msgType" $ fromString "bar"]
+parseFleetStatus :: Json -> Maybe FleetStatus
+parseFleetStatus json = do
+  obj <- toObject json
+  name <- M.lookup "name" obj >>= toString
+  statusStr <- M.lookup "status" obj >>= toString
+  status <- case statusStr of
+    "alive" -> Just Alive 
+    "destroyed" -> Just Destroyed
+    _ -> Nothing
+  pure $ FleetStatus { name: name, status: status }
+  
+derive instance genericClientMessage :: Generic ClientMessage
+instance showClientMessage :: Show ClientMessage where
+  show = gShow
+      
+instance encodeJsonClientMessage :: EncodeJson ClientMessage where
+  encodeJson Join = fromObject $ M.fromFoldable [msgType "join"]
+  encodeJson Ping = fromObject $ M.fromFoldable [msgType "ping"]
+  encodeJson (LaunchMissiles _data) = fromObject $ M.fromFoldable [msgType "launchMissiles"]
+  encodeJson (MoveFleet dirs) = fromObject $ M.fromFoldable [msgType "moveFleet"]
+  encodeJson SelfDestruct = fromObject $ M.fromFoldable [msgType "selfDestruct"]
+  
+msgType :: String -> Tuple String Json
+msgType t = Tuple "msgType" $ fromString t
 
-logMessage :: forall e. Message -> Eff (console :: CONSOLE | e) Unit
-logMessage msg = log $ show msg
-
-parseEvent :: UdpEvent -> Either String Message
+parseEvent :: UdpEvent -> Either String ServerMessage
 parseEvent (MessageEvent msg) = parseMessage msg
 parseEvent event = Left $ "Could not parse event: '" ++ show event ++ "'"
 
-parseMessage :: String -> Either String Message
+parseMessage :: String -> Either String ServerMessage
 parseMessage raw = (bimap errorMsg id $ jsonParser raw) >>= decodeJson
   where errorMsg msg = "Error parsing Json from message '" ++ raw ++ "': " ++ msg
--- src/SpaceBlitz/Server.purs
+++ src/SpaceBlitz/Server.purs
@@ -5,9 +5,10 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE(), log)
 import Data.Argonaut.Encode (encodeJson)
 import Data.Argonaut.Printer (printJson)
+import Data.Either (either)
 
 import SpaceBlitz.Dgram (UDP, createSocket, send)
-import SpaceBlitz.Message (Message(FooMessage), parseEvent)
+import SpaceBlitz.Message (ClientMessage(..), ServerMessage(..), parseEvent)
 
 type Config = {
   port :: Int,
@@ -21,9 +22,12 @@ config = {
   host: "127.0.0.1",
   hostPort: 3001
 }
+
+handleMessage :: forall eff. ServerMessage -> Eff (console :: CONSOLE, udp :: UDP | eff) Unit
+handleMessage msg = log $ show msg
   
-main :: forall e. Eff (console :: CONSOLE, udp :: UDP| e) Unit
+main :: forall eff. Eff (console :: CONSOLE, udp :: UDP| eff) Unit
 main = do 
-  socket <- createSocket config.port (parseEvent >>> show >>> log)
-  send socket config.host config.hostPort (printJson $ encodeJson FooMessage)
+  socket <- createSocket config.port (parseEvent >>> either log handleMessage)
+  send socket config.host config.hostPort (printJson $ encodeJson Ping)
   return unit
